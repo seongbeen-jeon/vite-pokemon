26702de520cc761253f2fa787094f1b85e07189d
unknown <wjstjdqls98@gmail.com>
2026-09-11 17:23:37 +0900

    불필요한 코드 정리

--- src/pages/BoxDetail.jsx
+++ src/pages/BoxDetail.jsx
@@ -3,8 +3,6 @@ import {Link} from 'react-router-dom';
 import {getCards} from "../services/cardService.js";
 import {useState, useEffect} from 'react';
 
-//const IMG_BASE_URL = import.meta.env.VITE_IMG_BASE_URL;
-
 //box 상세 페이지에서 카드가 몇 장 보일지 제한 / 테스트용 20장
 const limit = 20;
 
@@ -12,24 +10,12 @@ const limit = 20;
 function BoxDetail() {
     const set_Code = useParams().set_Code;
     const [cards, setCards] = useState([]);
-    const [loading, setLoading] = useState(true);
     
-
     useEffect(()=>{
         async function getCardsList(){  
             try{
                 const data = await getCards({set_code:set_Code, limit});
-                
-                /*//path data 가공
-                const newPathData = data.map((card)=>({
-                    ...card,
-                    image_path : card.image_path.startsWith("SV") ? `SV/${card.image_path.split("_")[0]}/${card.image_path}.webp` 
-                        : card.image_path.startsWith("S") ? `S/${card.image_path.split("_")[0]}/${card.image_path}.webp`
-                        : card.image_path.startsWith("M") ? `MEGA/${card.image_path.split("_")[0]}/${card.image_path}.webp`
-                        : card.image_path
-                }));
-                newPathData.map((card)=>{console.log ('cardid:', card.id)});*/
-
+            
                 console.log("box detail data : ",data);
                 setCards(data);
             
@@ -38,19 +24,10 @@ function BoxDetail() {
             }finally{
                 setLoading(false);
             }
-
         }
         getCardsList();
         
     },[]);
-   
-
-    /*
-    *   
-    기능 
-    1.필터 추가하기
-    */
-
 
     return(
     <>  
